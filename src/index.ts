a5a24a556830c581f4b50c2fbc9e652e7c0d8a0d
Frederick Johnson <fritzj96@gmail.com>
2020-11-24 07:44:11 -0600

    added: database connection creator

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import 'reflect-metadata';
+import { createConnection } from "typeorm";
 import { configureEnv } from './utils/configure-env';
 
 // load + validate environment variables
 const main = async () => {
   configureEnv();
+  await createConnection({ type: "postgres", url: process.env.DATABASE_URL, synchronize: true });
 };
 
 main();
