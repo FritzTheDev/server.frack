7ee6ca9bfc93e0428bbd7fa2b2176220ca742280
Frederick Johnson <fritzj96@gmail.com>
2020-11-24 07:56:26 -0600

    added: NODE_ENV to valid env vars

--- src/utils/configure-env.ts
+++ src/utils/configure-env.ts
@@ -1,9 +1,10 @@
 import { config as loadEnv } from "dotenv";
-import { cleanEnv, host, port, url } from 'envalid';
+import { cleanEnv, host, port, str, url } from 'envalid';
 
 export const configureEnv = (): void => {
   loadEnv();
   cleanEnv(process.env, {
+    NODE_ENV: str(),
     PORT: port(),
     DATABASE_URL: url(),
     SERVER_HOST: host(),
