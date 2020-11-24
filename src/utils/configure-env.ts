0969f6dfda78efef0f9b349a434fcfb9aa9408ac
Frederick Johnson <fritzj96@gmail.com>
2020-11-24 07:40:00 -0600

    added: DATABASE_URL to env validation

--- src/utils/configure-env.ts
+++ src/utils/configure-env.ts
@@ -1,10 +1,11 @@
 import { config as loadEnv } from "dotenv";
-import { cleanEnv, host, port } from 'envalid';
+import { cleanEnv, host, port, url } from 'envalid';
 
 export const configureEnv = (): void => {
   loadEnv();
   cleanEnv(process.env, {
     PORT: port(),
+    DATABASE_URL: url(),
     SERVER_HOST: host(),
     CLIENT_HOST: host(),
   });
