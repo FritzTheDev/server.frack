2696385d4734e316a5be53e032e139561e775fac
Frederick Johnson <fritzj96@gmail.com>
2020-11-24 08:00:41 -0600

    added: logger middleware & cors middleware

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,26 @@
 import 'reflect-metadata';
-import { createConnection } from "typeorm";
+import cors from 'cors';
+import Express from 'express';
+import { createConnection } from 'typeorm';
 import { configureEnv } from './utils/configure-env';
+import morgan from 'morgan';
 
 // load + validate environment variables
 const main = async () => {
   configureEnv();
-  await createConnection({ type: "postgres", url: process.env.DATABASE_URL, synchronize: true });
+  // connect to the database
+  await createConnection({ type: 'postgres', url: process.env.DATABASE_URL, synchronize: true });
+  console.info('Database Connection Established Successfully');
+
+  // create & configure expresss app
+  const app = Express();
+
+  // global middleware
+  app.use(cors({ origin: process.env.CLIENT_HOST }));
+  // logs more if in development
+  const logSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'dev';
+  app.use(morgan(logSetting));
+  console.log(`Logging started in ${logSetting} mode`);
 };
 
 main();
