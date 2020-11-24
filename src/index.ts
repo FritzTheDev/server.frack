ec8ef5a652f9547b357c5f34aee84956a29bb6d4
Frederick Johnson <fritzj96@gmail.com>
2020-11-24 08:05:48 -0600

    comments

--- src/index.ts
+++ src/index.ts
@@ -5,22 +5,25 @@ import { createConnection } from 'typeorm';
 import { configureEnv } from './utils/configure-env';
 import morgan from 'morgan';
 
-// load + validate environment variables
 const main = async () => {
+  // load + validate environment variables
   configureEnv();
   // connect to the database
   await createConnection({ type: 'postgres', url: process.env.DATABASE_URL, synchronize: true });
-  console.info('Database Connection Established Successfully');
+  console.info(`INFO: Database Connection Established Successfully`);
 
   // create & configure expresss app
   const app = Express();
 
   // global middleware
+  // logs the cors configuration in case you forget to set it correctly.
   app.use(cors({ origin: process.env.CLIENT_HOST }));
+  console.info(`INFO: CORS configured to allow access from ${process.env.CLIENT_HOST}`);
+
   // logs more if in development
   const logSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'dev';
   app.use(morgan(logSetting));
-  console.info(`Logger started in ${logSetting} mode`);
+  console.info(`INFO: Logger started in ${logSetting} mode`);
 };
 
 main();
